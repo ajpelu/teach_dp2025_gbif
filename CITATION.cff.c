d9462c806e3a475a0dd83e8991a7b24b7f808465
ajpelu <ajpelu@gmail.com>
2025-11-04 00:08:19 +0100

    update repo and add citation

---
+++ CITATION.cff.c
@@ -0,0 +1,32 @@
+# This CITATION.cff file was generated with cffinit.
+# Visit https://bit.ly/cffinit to generate yours today!
+
+cff-version: 1.2.0
+title: >-
+  Módulo Artículo de Datos. Curso-Taller GBIF: Estandarización, documentación y
+  publicación de datos de seguimiento de biodiversidad 
+message: >-
+  If you use this software, please cite it using the
+  metadata from this file.
+type: software
+authors:
+  - email: ajpelu@gmail.com
+    orcid: 'https://orcid.org/0000-0002-1747-0469'
+    given-names: Antonio J.
+    family-names: Pérez-Luque
+identifiers:
+  - type: doi
+    value: '10.5281/zenodo.17516296'
+repository-code: 'https://ajpelu.github.io/teach_dp2025_gbif/'
+abstract: >-
+  Material for the "Data Papers" module within the course
+  GBIF.ES Workshop: Standardisation, documentation and publication of 
+  biodiversity monitoring data (In spanish), hosted by GBIF-Spain node,
+  held in Sevilla (Spain) 28-30 March 2025. More info at
+  https://gbif.es/talleres/taller-gbif-es-estandarizacion-documentacion-y-publicacion-de-datos-de-programas-de-seguimiento-de-biodiversidad/
+keywords:
+  - data papers
+  - biodiviersity data
+  - publishing
+license: CC-BY-4.0
+date-released: '2025-11-01'
