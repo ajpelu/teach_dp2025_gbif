b9b686e26111070b2dc7f759e6c25ff2a7ec0cbd
ajpelu <ajpelu@gmail.com>
2025-11-04 00:22:51 +0100

    add pdf

---
+++ genera_pdf.md.R
@@ -0,0 +1,14 @@
+# To print from url to pdf (reveals)
+
+-1. Use [decktape](https://github.com/astefanutti/decktape)
+-2. Install locally 
+-3. Run the following code
+
+```
+decktape url filename.pdf
+```
+-4. Example 
+```
+cd Desktop
+decktape https://ajpelu.github.io/teach_dp/ipt2dp.html ipt2dp.pdf
+```
